48192575df80a878192471a1e78070ce87bac15c
kur <rhdnfka94@gmail.com>
2018-07-09 11:54:06 +0900

    Add global thresholding interface

--- seg_nucleus.py
+++ seg_nucleus.py
@@ -1,5 +1,5 @@
 #123456789012345678901234567890123456789012345678901234567890 # 60 limit
-import os
+import os, sys
 import cv2
 from matplotlib import pyplot as plt
 plt.ion()
@@ -16,19 +16,23 @@ def look_and_feel(image, window_title):
             key == ord('k')):
             return chr(key)
 
+def mod_add(augend,addend, mod_n):
+    return ((augend + addend) + mod_n) % mod_n
+
 def gray2bgr(grayimg):
     return cv2.cvtColor(grayimg, cv2.COLOR_GRAY2RGB) 
+
 data_dir = '../data-pnu/'
 imgnames = os.listdir(data_dir)
 imgpaths = map(lambda path:os.path.join(data_dir,path),imgnames)
-imgs = map(lambda p: cv2.imread(p,0), imgpaths)
-for img in imgs:
-    # plot histogram
-    #hist = cv2.calcHist([img],[0],None,[256],[0,256])
-    #plt.hist(img.ravel(),256,[0,256]); plt.show()
+imgs = list(map(lambda p: cv2.imread(p,0), imgpaths))
 
-    # return binary mask
-    _,not_nucleuses = cv2.threshold(img,70,1,cv2.THRESH_BINARY)
+idx = 0
+threshold = 70
+is_global = True
+while True:
+    img = imgs[idx]
+    _,not_nucleuses = cv2.threshold(img,threshold,1,cv2.THRESH_BINARY)
     '''
     not_nucleuses = cv2.adaptiveThreshold(
                    img,1,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
@@ -44,14 +48,28 @@ for img in imgs:
     img4check = holed_img + nucleuses
 
     cv2.imshow('img', img)
-    cv2.imshow('nucleuses', cv2.bitwise_not(nucleuses))
+    #cv2.imshow('nucleuses', cv2.bitwise_not(nucleuses))
     cv2.imshow('not nucleuses', not_nucleuses*255)
     #cv2.imshow('holed_img', holed_img)
-    cv2.imshow('image for checking', img4check)
-    cv2.waitKey(0)
-    print('----')
 
-    plt.close()
+    cmd = look_and_feel(img4check,'image for checking')
+    if cmd == 'a' or cmd == 'd':
+        val = -1 if cmd == 'a' else +1
+        idx = mod_add(idx, val, len(imgs))
+    elif cmd == 'j' or cmd == 'k':
+        val = -1 if cmd == 'j' else +1
+        threshold += val
+    elif cmd == 'q':
+        sys.exit(0)
+
+    print('idx =',idx,'threshold=',threshold)
+
+    #plt.close()
 
 # use threshold adaptive to histogram!
 # and then dilation?
+
+# plot histogram
+#hist = cv2.calcHist([img],[0],None,[256],[0,256])
+#plt.hist(img.ravel(),256,[0,256]); plt.show()
+# return binary mask
