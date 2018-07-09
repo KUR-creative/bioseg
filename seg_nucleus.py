6c6829a430486656990db5f5fe31bacc5e318989
kur <rhdnfka94@gmail.com>
2018-07-09 11:19:27 +0900

    last

--- seg_nucleus.py
+++ seg_nucleus.py
@@ -4,6 +4,8 @@ import cv2
 from matplotlib import pyplot as plt
 plt.ion()
 
+def gray2bgr(grayimg):
+    return cv2.cvtColor(grayimg, cv2.COLOR_GRAY2RGB) 
 data_dir = '../data-pnu/'
 imgnames = os.listdir(data_dir)
 imgpaths = map(lambda path:os.path.join(data_dir,path),imgnames)
@@ -14,30 +16,26 @@ for imgpath in imgpaths:
     #hist = cv2.calcHist([img],[0],None,[256],[0,256])
     #plt.hist(img.ravel(),256,[0,256]); plt.show()
 
-    threshold = 70
-    _,thr1 = cv2.threshold(img,70,1,cv2.THRESH_BINARY)
-    thr2 = cv2.adaptiveThreshold(
-             img, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-             cv2.THRESH_BINARY,91,2
-           )
-    holed_img = cv2.cvtColor(img * thr1, cv2.COLOR_GRAY2RGB) 
+    # return binary mask
+    _,not_nucleuses = cv2.threshold(img,70,1,cv2.THRESH_BINARY)
+    '''
+    not_nucleuses = cv2.adaptiveThreshold(
+                   img,1,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                   cv2.THRESH_BINARY,501,30
+                 )
+    '''
+    holed_img = gray2bgr(img * not_nucleuses)
 
-    cv2.imshow('img', img)
-    cv2.imshow('thresh1', thr1*255)
-    cv2.imshow('holed_img', holed_img)
-    #cv2.imshow('thresh2',thr2)
-    rgb_img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-    #cv2.imshow('green_thr1',thr1*255);cv2.waitKey(0)
-    nucleuses = cv2.bitwise_not(thr1*255)
-    #cv2.imshow('green_thr1',nucleuses);cv2.waitKey(0)
-    nucleuses = cv2.cvtColor(nucleuses, cv2.COLOR_GRAY2RGB) 
-    #cv2.imshow('green_thr1',nucleuses);cv2.waitKey(0)
+    nucleuses = gray2bgr(cv2.bitwise_not(not_nucleuses*255))
     nucleuses[:,:,0] = 0
     nucleuses[:,:,1] = 0
-    #nucleuses[:,:,2] = 0
-    #cv2.imshow('green_thr1',nucleuses);cv2.waitKey(0)
-    #cv2.imshow('green_thr1',nucleuses)
+
     img4check = holed_img + nucleuses
+
+    cv2.imshow('img', img)
+    cv2.imshow('nucleuses', cv2.bitwise_not(nucleuses))
+    cv2.imshow('not nucleuses', not_nucleuses*255)
+    #cv2.imshow('holed_img', holed_img)
     cv2.imshow('image for checking', img4check)
     cv2.waitKey(0)
     print('----')
