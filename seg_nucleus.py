707901df2913bb8ba69f9352f29d5e9e7ab05ae4
kur <rhdnfka94@gmail.com>
2018-07-09 12:04:37 +0900

    Add local thresholding

--- seg_nucleus.py
+++ seg_nucleus.py
@@ -27,42 +27,76 @@ imgnames = os.listdir(data_dir)
 imgpaths = map(lambda path:os.path.join(data_dir,path),imgnames)
 imgs = list(map(lambda p: cv2.imread(p,0), imgpaths))
 
+#global_threshold = True
 idx = 0
 threshold = 70
-is_global = True
+
+global_threshold = False
+block_size = 17 # odd number!
+C = 6
+#adaptive_method = cv2.ADAPTIVE_THRESH_GAUSSIAN_C
+adaptive_method = cv2.ADAPTIVE_THRESH_MEAN_C
 while True:
     img = imgs[idx]
-    _,not_nucleuses = cv2.threshold(img,threshold,1,cv2.THRESH_BINARY)
-    '''
-    not_nucleuses = cv2.adaptiveThreshold(
-                   img,1,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
-                   cv2.THRESH_BINARY,501,30
-                 )
-    '''
-    holed_img = gray2bgr(img * not_nucleuses)
+    if global_threshold:
+        _,not_nucleuses = cv2.threshold(img,threshold,1,cv2.THRESH_BINARY)
+        holed_img = gray2bgr(img * not_nucleuses)
 
-    nucleuses = gray2bgr(cv2.bitwise_not(not_nucleuses*255))
-    nucleuses[:,:,0] = 0
-    nucleuses[:,:,1] = 0
+        nucleuses = gray2bgr(cv2.bitwise_not(not_nucleuses*255))
+        nucleuses[:,:,0] = 0
+        nucleuses[:,:,1] = 0
 
-    img4check = holed_img + nucleuses
+        img4check = holed_img + nucleuses
 
-    cv2.imshow('img', img)
-    #cv2.imshow('nucleuses', cv2.bitwise_not(nucleuses))
-    cv2.imshow('not nucleuses', not_nucleuses*255)
-    #cv2.imshow('holed_img', holed_img)
+        cv2.imshow('img', img)
+        #cv2.imshow('nucleuses', cv2.bitwise_not(nucleuses))
+        cv2.imshow('not nucleuses', not_nucleuses*255)
+        #cv2.imshow('holed_img', holed_img)
 
-    cmd = look_and_feel(img4check,'image for checking')
-    if cmd == 'a' or cmd == 'd':
-        val = -1 if cmd == 'a' else +1
-        idx = mod_add(idx, val, len(imgs))
-    elif cmd == 'j' or cmd == 'k':
-        val = -1 if cmd == 'j' else +1
-        threshold += val
-    elif cmd == 'q':
-        sys.exit(0)
+        cmd = look_and_feel(img4check,'image for checking')
+        if cmd == 'a' or cmd == 'd':
+            val = -1 if cmd == 'a' else +1
+            idx = mod_add(idx, val, len(imgs))
+        elif cmd == 'j' or cmd == 'k':
+            val = -1 if cmd == 'j' else +1
+            threshold += val
+        elif cmd == 'q':
+            sys.exit(0)
 
-    print('idx =',idx,'threshold=',threshold)
+        print('idx =',idx,'threshold =',threshold)
+    else:
+        not_nucleuses = cv2.adaptiveThreshold(
+                          img,1,adaptive_method,
+                          cv2.THRESH_BINARY,block_size,C
+                        )
+        holed_img = gray2bgr(img * not_nucleuses)
+
+        nucleuses = gray2bgr(cv2.bitwise_not(not_nucleuses*255))
+        nucleuses[:,:,0] = 0
+        nucleuses[:,:,1] = 0
+
+        img4check = holed_img + nucleuses
+
+        cv2.imshow('img', img)
+        #cv2.imshow('nucleuses', cv2.bitwise_not(nucleuses))
+        cv2.imshow('not nucleuses', not_nucleuses*255)
+        #cv2.imshow('holed_img', holed_img)
+
+        cmd = look_and_feel(img4check,'image for checking')
+        if cmd == 'a' or cmd == 'd':
+            val = -1 if cmd == 'a' else +1
+            idx = mod_add(idx, val, len(imgs))
+        elif cmd == 'j' or cmd == 'k':
+            val = -1 if cmd == 'j' else +1
+            C += val
+        elif cmd == 's' or cmd == 'w':
+            val = -2 if cmd == 'j' else +2
+            block_size += val
+        elif cmd == 'q':
+            sys.exit(0)
+
+        print('idx =',idx,'block_size =',block_size,'C =',C)
+
 
     #plt.close()
 
